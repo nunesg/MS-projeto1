fe4c7280b74e55d97953d83cc573ecc82336e13e
Gustavo Candido <gustavo.candido@pipefy.com>
2021-05-03 08:31:17 -0300

    config file

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from simulation import Simulation
+import configparser
 import generators
+
+from numpy import random as npr
+
 
 
 def main():
+    config = configparser.ConfigParser()
+    config.read('config.env')
+
     generator = generators.DummyGenerator(5, 10)
     simulation = Simulation(generator, generator)
     simulation.simulate()
